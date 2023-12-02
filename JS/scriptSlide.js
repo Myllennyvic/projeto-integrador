1f9140e71237a110b6964461aafc32e3f019c7df
Myllennyv <138785499+Myllennyvic@users.noreply.github.com>
2023-12-01 23:39:53 -0300

    atualizações, homefeito

--- JS/scriptSlide.js
+++ JS/scriptSlide.js
@@ -1,24 +1,3 @@
-// const userButton = document.querySelector('.user-button');
-// const professionalButton = document.querySelector('.professional-button');
-
-// userButton.addEventListener('click', () => {
-//     window.location.href = 'cadastro-usuario.html';
-// });
-
-// professionalButton.addEventListener('click', () => {
-//     window.location.href = 'cadastro-profissional.html';
-// });
-
-// var ul = document.querySelector('nav ul');
-// var menuBtn = document.querySelector('.menu-btn i');
-
-// function menuShow() {
-//     if (ul.classList.contains('open')) {
-//         ul.classList.remove('open');
-//     }else{
-//         ul.classList.add('open');
-//     }
-// }
 let slideIndex = 1;
 showSlides(slideIndex);
 
